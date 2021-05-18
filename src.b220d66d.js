f3a1adef22325c570060fd1d2feddff613025d07
Yana-Filippova <74433621+Yana-Filippova@users.noreply.github.com>
2021-05-18 00:02:18 +0000

    Deploying to gh-pages from @ Yana-Filippova/goit-js-hw-11-promisification@93fff744336c27411ad34fa0aee4d14f58e62644 🚀

--- src.b220d66d.js
+++ src.b220d66d.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./css/common.css");
 },{"./css/common.css":"en75"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11-promisification/src.e013ce97.js.map
+//# sourceMappingURL=/goit-js-hw-11-promisification/src.b220d66d.js.map
